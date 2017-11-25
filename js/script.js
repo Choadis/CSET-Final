2619936814d0b2931774f570ad9978bd2c62c6dd
colelabar <31549550+colelabar@users.noreply.github.com>
2017-11-25 13:32:40 -0500

    Added in Saturday to the date script, and changed the page title for the logout screen

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(() => {
   })
 });
 var d = new Date();
-var days = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"];
+var days = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
 var months = ["JANUARY", "FEBUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
 $(document).ready(() => {
   $('.date').text(days[d.getDay()]+", "+months[d.getMonth()]+" "+ d.getDay()+", "+d.getFullYear());
