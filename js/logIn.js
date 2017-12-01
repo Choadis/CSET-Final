dd3c26382db57e11b0b5ae232d0cfbc7b2adfc60
Stefen Dahlgren <31549513+Choadis@users.noreply.github.com>
2017-12-01 16:16:31 -0500

    pushing a broken function

--- js/logIn.js
+++ js/logIn.js
@@ -1,8 +1,9 @@
-var email = $('#email').val();
-var password = $('#password').val();
-
-$(document).ready(() => {
-  $('#logInButton').on('click', () =>{
-
-  })
-}
+// $(document).ready(() => {
+// $('#logInButton').on('click', () =>{
+//     var email = $('#email').val();
+//     var password = $('#password').val();
+//     var check = email.slice(-19);
+//     if (check === '@stevenscollege.edu' && password.strlength > 1)
+//
+//   })
+// })
