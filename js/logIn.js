dd81f5d990ace67a55cb4797599cbecbf83cba7c
Stefen Dahlgren <31549513+Choadis@users.noreply.github.com>
2017-12-04 12:22:31 -0500

    made the thingy

--- js/logIn.js
+++ js/logIn.js
@@ -1,9 +1,11 @@
-// $(document).ready(() => {
-// $('#logInButton').on('click', () =>{
-//     var email = $('#email').val();
-//     var password = $('#password').val();
-//     var check = email.slice(-19);
-//     if (check === '@stevenscollege.edu' && password.strlength > 1)
-//
-//   })
-// })
+$(document).ready(() => {
+$('#logInButton').on('click', () =>{
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var check = email.slice(-19);
+    if (check === '@stevenscollege.edu' && password.length > 1)
+    console.log('this is working');
+    if (check != '@stevenscollege.edu' || password.length < 1)
+    console.log('Incorrect Email address or password');
+  })
+})
